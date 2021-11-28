88f79f53d03d86686617a6945af732c194f7f5d6
bagebear <bagebear@gmail.com>
2021-11-28 22:40:52 +0800

    feat: keep get currentUser via api

--- src/apis/restaurants.js
+++ src/apis/restaurants.js
@@ -6,7 +6,7 @@ export default {
   getRestaurants({ page, categoryId }) {
 
     const searchParams = new URLSearchParams({ page, categoryId })
-    console.log(searchParams)
+    
     return apiHelper.get(`/restaurants?${searchParams.toString()}`, {
       headers: { Authorization: `Bearer ${getToken()}` }
     })
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import userAPI from './../apis/users'
 
 Vue.use(Vuex)
 
@@ -15,7 +16,7 @@ export default new Vuex.Store({
     isAuthenticated: false
   },
   mutations: {
-    setCurrentUser (state, currentUser) {
+    setCurrentUser(state, currentUser) {
       state.currentUser = {
         ...state.currentUser,
         ...currentUser
@@ -24,7 +25,26 @@ export default new Vuex.Store({
     }
   },
   actions: {
+    async fetchCurrentUser({ commit }) {
+      try {
+        const { data } = await userAPI.getCurrentUser()
+
+        if (data.status === 'error') {
+          throw new Error(data.message)
+        }
+
+        const { id, name, email, image, isAdmin } = data
+
+        commit('setCurrentUser', {
+          id, name, email, image, isAdmin
+        })
+
+      } catch (error) {
+        console.error(error.message)
+      }
+    }
   },
   modules: {
   }
 })
+
