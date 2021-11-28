95ba301df055592fea3ce44808563e256a308ee7
bagebear <bagebear@gmail.com>
2021-11-28 18:49:10 +0800

    feat: add CRUD functions of admin categories with api

--- src/views/AdminCategories.vue
+++ src/views/AdminCategories.vue
@@ -46,6 +46,7 @@
               v-model="category.name"
               type="text"
               class="form-control"
+              @keyup.enter="updateCategory({ categoryId: category.id, name: category.name })"
             />
             <span
               v-show="category.isEditing"
@@ -89,83 +90,99 @@
 </template>
 
 <script>
-import { v4 as uuidv4 } from "uuid";
 import AdminNav from "@/components/AdminNav";
-//  2. 定義暫時使用的資料
-const dummyData = {
-  categories: [
-    {
-      id: 1,
-      name: "中式料理",
-      createdAt: "2019-06-22T09:00:43.000Z",
-      updatedAt: "2019-06-22T09:00:43.000Z",
-    },
-    {
-      id: 2,
-      name: "日本料理",
-      createdAt: "2019-06-22T09:00:43.000Z",
-      updatedAt: "2019-06-22T09:00:43.000Z",
-    },
-    {
-      id: 3,
-      name: "義大利料理",
-      createdAt: "2019-06-22T09:00:43.000Z",
-      updatedAt: "2019-06-22T09:00:43.000Z",
-    },
-    {
-      id: 4,
-      name: "墨西哥料理",
-      createdAt: "2019-06-22T09:00:43.000Z",
-      updatedAt: "2019-06-22T09:00:43.000Z",
-    },
-  ],
-};
+import adminAPI from "./../apis/admin";
+import { Toast } from "./../utils/helpers";
 
 export default {
   name: "AdminCategories",
   components: {
     AdminNav,
   },
-  // 3. 定義 Vue 中使用的 data 資料
   data() {
     return {
       categories: [],
       newCategoryName: "",
     };
   },
-  // 5. 調用 `fetchCategories` 方法
   created() {
     this.fetchCategories();
   },
   methods: {
-    // 4. 定義 `fetchCategories` 方法，把 `dummyData` 帶入 Vue 物件
-    fetchCategories() {
-      this.categories = dummyData.categories.map((category) => ({
-        ...category,
-        isEditing: false,
-        nameCached: "",
-      }));
+    async fetchCategories() {
+      try {
+        const { data } = await adminAPI.categories.get();
+        this.categories = data.categories.map((category) => ({
+          ...category,
+          isEditing: false,
+          nameCached: "",
+        }));
+      } catch (error) {
+        Toast.fire({
+          icon: "error",
+          title: "無法取得餐廳類別，請稍後再試",
+        });
+      }
     },
-    createCategory() {
-      //TODO 透過API向後端伺服器取得餐廳類別
-      this.categories.push({
-        id: uuidv4(),
-        name: this.newCategoryName,
-      });
 
-      this.newCategoryName = "";
+    async createCategory() {
+      try {
+        const { data } = await adminAPI.categories.create({
+          name: this.newCategoryName,
+        });
+
+        if (data.status !== "success") {
+          throw new Error(data.message);
+        }
+
+        this.newCategoryName = "";
+        this.fetchCategories();
+      } catch (error) {
+        console.log(error);
+        Toast.fire({
+          icon: "warning",
+          title: "無法新增餐廳類別，請稍後再試",
+        });
+      }
     },
-    deleteCategory(categoryId) {
-      //TODO 透過API像後端伺服器刪除餐廳類別
-      this.categories = this.categories.filter(
-        (category) => category.id !== categoryId
-      );
+
+    async deleteCategory(categoryId) {
+      try {
+        const { data } = await adminAPI.categories.delete({ categoryId });
+
+        if (data.status !== "success") {
+          throw new Error(data.message);
+        }
+
+        this.fetchCategories();
+      } catch (error) {
+        Toast.fire({
+          icon: "warning",
+          title: "無法刪除餐廳類別，請稍後再試",
+        });
+      }
     },
-    updateCategory({ categoryId, name }) {
-      // TODO: 透過 API 去向伺服器更新餐廳類別名稱
+
+    async updateCategory({ categoryId, name }) {
+      try {
+        const { data } = await adminAPI.categories.update({ categoryId, name });
+
+        if (data.status !== "success") {
+          throw new Error(data.message);
+        }
+
+        this.fetchCategories();
+
+      } catch(error) {
+        Toast.fire({
+          icon: "warning",
+          title: "無法編輯餐廳類別，請稍後再試",
+        });
+      }
       this.toggleIsEditing(categoryId);
       console.log("update ", categoryId, "into ", name);
     },
+
     toggleIsEditing(categoryId) {
       this.categories = this.categories.map((category) => {
         if (category.id === categoryId) {
